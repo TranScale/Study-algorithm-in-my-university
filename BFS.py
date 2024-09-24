c01b9c7bdb06723255c5ea1a5ee3c7cbfa2f7ed2
Scale <tranhai8438424@gmail.com>
2024-09-24 21:44:26 +0700

    Thuật toán BFS - Breadth First Search

--- BFS.py
+++ BFS.py
@@ -1,5 +1,17 @@
 #Trần Viết Hải - 225051529, BFS
+'''
+Thuật toán này là thuật toán tìm kiếm theo chiều rộng, chia đồ thị thành các level rồi tìm dần
 
+vd:         A
+           / \
+          B   C -- level 1
+         / \
+        D   E   -- level 2
+       /
+      F         -- level 3
+
+Như đồ thị trên thì chia thành 3 level(1,2,3), sau đó duyệt theo từng level
+'''
 from collections import deque
 
 
@@ -29,5 +41,6 @@ def bfs(graph,start):
         for neighbour in graph[node]:
             if neighbour not in visited:
                 queue.append(neighbour)
+
 
 print(bfs(graph,'M'))
